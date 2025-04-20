ad6b6c932d36423920bb1b66a2e339d0b6406bb5
hanwj75 <wjws9608@gmail.com>
2025-04-21 04:44:29 +0900

    Update : "swagger 적용"
    메인 서버파일에 swagger 적용

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import express from "express";
 import config from "./src/config/config.js";
 import initServer from "./src/init/index.js";
 import userRouter from "./src/routes/user.router.js";
+import { specs, swaggerUi } from "./src/config/swagger.js";
 
 const app = express();
 const { server } = config;
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api", [userRouter]);
-
 initServer()
   .then(() => {
     app.listen(server.PORT, server.HOST, () => {
