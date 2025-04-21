220e830a15d427b0d205979ea86adea4374df9b4
hanwj75 <wjws9608@gmail.com>
2025-04-21 17:52:35 +0900

    Update : "JWT 인증요구 + post swagger추가"

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -7,15 +7,29 @@ const options = {
     info: {
       title: "회원 API 문서",
       version: "1.0.0",
-      description: "회원 관련 기능 API 명세서입니다.",
+      description: "회원 관련 기능 API 문서입니다.",
     },
     servers: [
       {
-        url: "http://localhost:3333",
+        url: "http://localhost:3333", // API 서버 URL
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // JWT 토큰 형식
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [], // 모든 API에 기본적으로 JWT 인증을 요구
       },
     ],
   },
-  apis: ["./src/docs/user.swagger.js" /*"./src/docs/post.swagger.js"*/],
+  apis: ["./src/docs/user.swagger.js", "./src/docs/post.swagger.js"],
 };
 
 const specs = swaggerJsdoc(options);
